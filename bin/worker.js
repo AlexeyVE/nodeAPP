a8e83d0fd93d5d32d8c0e79ca7676ba859c9b49c
Alexey Efimenko <sle8882@gmail.com>
2017-10-10 12:23:14 +0300

    Basic server

--- bin/worker.js
+++ bin/worker.js
@@ -2,6 +2,16 @@ var express = require('express');
 
 var app = express();
 
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
+app.use('/public',express.static(path.join(__dirname,'../public')));
+
+app.use(bodyParser.json({
+    limit:"10kb"
+}));
+
 app.get('/',(req,res,next)=> {
     res.send('Hello, World!');
 });
